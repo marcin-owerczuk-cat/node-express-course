4d8c75adb43c435817b7d131279b4913ea6d6c2e
Marcin Owerczuk <marcin.owerczuk@checkatrade.com>
2024-04-05 14:48:17 +0200

    numeric filters

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,7 +1,17 @@
 const Products = require('../models/Product');
 
 const getAllProducts = async (req, res) => {
-  const { featured, name, company, sort }  = req.query;
+  const {
+    featured,
+    name,
+    company,
+    sort,
+    select,
+    limit,
+    page,
+    numericFilters
+  }  = req.query;
+
   console.log(featured);
   const queryObject = {};
   if (featured) {
@@ -16,14 +26,43 @@ const getAllProducts = async (req, res) => {
     queryObject.company = company;
   }
 
+  if(numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '<': '$lt',
+      '<=': '$lte',
+      '=': '$eq',
+    }
 
-  const query = Products.find(queryObject);
+    const regEx = /\b(>|>=|=|<|<=)\b/g;
+    const numericFields = ['price', 'rating'];
+    const filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+    filters.split(',').forEach((filter) => {
+      const [field, operator, value] = filter.split('-');
+      if(numericFields.includes(field))
+        queryObject[field] = {[operator]: Number(value)};
+    })
 
-  if(sort) {
-    query.sort(sort.split(',').join(' '));
   }
-  const products = await query;
 
+  console.log('---');
+  console.log(queryObject);
+  const sortQuery = sort ? sort.split(',').join(' ') : '-createdAt';
+  let query = Products.find(queryObject).sort(sortQuery);
+
+  if(select) {
+    query = query.select(select.split(',').join(' '));
+  }
+
+  const pageNumber = Number(page) || 1;
+  const limitNumber = Number(limit) || 10;
+  const skipNumber = (pageNumber - 1) * limitNumber;
+
+
+  query = query.skip(skipNumber).limit(limitNumber);
+
+  const products = await query;
   res.status(200).json({nbHits: products.length, products});
 }
 
