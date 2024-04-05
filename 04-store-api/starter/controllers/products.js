757cdd0c2dab404ab9466cbe80835b5ca60cad00
Marcin Owerczuk <marcin.owerczuk@checkatrade.com>
2024-04-05 13:03:04 +0200

    Products filtering

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,8 +1,22 @@
+const Products = require('../models/Product');
 
 const getAllProducts = async (req, res) => {
-  res.json({
-    message: 'All products fetched successfully',
-  })
+  const { featured, name, company}  = req.query;
+  console.log(featured);
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true';
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+  const products = await Products.find(queryObject);
+  res.status(200).json({nbHits: products.length, products});
 }
 
 module.exports = {
