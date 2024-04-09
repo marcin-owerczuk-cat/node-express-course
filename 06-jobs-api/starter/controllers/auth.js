2dcac655f80f53179f40afb4b9511a8e81fc257d
Marcin Owerczuk <marcin.owerczuk@checkatrade.com>
2024-04-09 16:10:54 +0200

    auth and jobs endpoints with JWT validation

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -2,42 +2,27 @@ const Users = require('../models/user');
 const { StatusCodes } = require('http-status-codes');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const CustomError = require('../errors');
+const {BadRequestError , UnauthenticatedError} = require('../errors');
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    console.log(hashedPassword);
-    const user = await Users.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-    console.log(user);
-    res.status(StatusCodes.CREATED).json(user);
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  const user = await Users.create({...req.body});
+  res.status(StatusCodes.CREATED).json(user);
 }
 
 const login = async (req, res) => {
   const { email, password } = req.body;
 
   if(!email || !password) {
-    throw new CustomError.BadRequestError('Please provide email and password');
+    throw new BadRequestError('Please provide email and password');
   }
 
   const user = await Users.findOne({email});
 
   if(!user) {
-    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    throw new UnauthenticatedError('Invalid Credentials');
   }
   if(!await bcrypt.compare(password, user.password)) {
-    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    throw new UnauthenticatedError('Invalid Credentials');
   }
 
   const token = jwt.sign({ id: user._id, name: user.name, email: user.email}, process.env.JWT_SECRET, {expiresIn: '1d'})
--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -16,9 +17,13 @@ const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide password'],
-    minlength: 20,
-    maxlength: 401,
+    minlength: 7,
   }
+})
+
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 })
 
 module.exports = mongoose.model('User', UserSchema);
